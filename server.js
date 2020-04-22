480044f818a41117cd05381f47ec5d73665e57fa
spatiality-dc <dan@dinosaurmedia.org>
2020-04-22 22:17:19 +1000

    cleaned up ports and routes

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var express = require("express");
 var app = express();
 
 // Sets an initial port. We"ll use this later in our listener
-var PORT = process.env.PORT || 8080;
+var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
@@ -34,6 +34,4 @@ require("./routes/htmlRoutes")(app);
 // The below code effectively "starts" our server
 // =============================================================================
 
-app.listen(PORT, function() {
-  console.log("App listening on PORT: " + PORT);
-});
+app.listen(PORT, () => console.log("App listening on PORT: " + PORT));
