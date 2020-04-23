26b3bbfe2c3d3bb187bc0d948837e167f266ea7d
spatiality-dc <dan@dinosaurmedia.org>
2020-04-23 16:19:59 +1000

    slight amends serverjs to fix heroku

--- server.js
+++ server.js
@@ -13,12 +13,13 @@ var express = require("express");
 // Tells node that we are creating an "express" server
 var app = express();
 
-// Sets an initial port. We"ll use this later in our listener
+// Sets an initial port. We'll use this later in our listener
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static("public"));
 
 // ================================================================================
 // ROUTER
